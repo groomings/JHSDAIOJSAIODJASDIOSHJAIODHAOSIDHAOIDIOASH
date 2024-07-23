931ab2ccdfb319a68ad275965b602a7d0873f001
exhorting <birdy3ager@gmail.com>
2024-07-23 03:34:56 +0300

    Update buttons.js

--- static/buttons.js
+++ static/buttons.js
@@ -1,7 +1,11 @@
 function invite() {
-	window.open("https://discord.com/api/oauth2/authorize?","__blank")
+    window.location.href = 'https://discord.com/oauth2/authorize?client_id=1262930991597621343&scope=bot&permissions=YOUR_PERMISSIONS';
 }
 
 function supportServer() {
-	window.open("https://discord.gg/iNviTEcoDe","__blank")
+    window.location.href = 'https://discord.gg/tattoo';
 }
+
+// Example of setting server and user count dynamically
+document.getElementById('serverCount').innerText = '1234'; // Replace with dynamic data
+document.getElementById('usersCount').innerText = '56789'; // Replace with dynamic data
